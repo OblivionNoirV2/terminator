db99da6bea20b10a4947c9357058e73829b53258
OblivionNoirV2 <roslyndo79@gmail.com>
2024-03-05 23:07:13 -0600

    refactor

--- terminator.cpp
+++ terminator.cpp
@@ -5,46 +5,67 @@
 using namespace std;
 
 string check_os(); 
-string read_config();
+bool read_config(size_t found);
 
 void kill_programs(string check_os_result, char* death_row[]); //recieves os and list of programs to kill (may be a list if this is started with multiple offenders running)
 
 //-l for load kill profile, -d for delete kill profiles(array), -sd for set default, -a for auto (uses default if it exists), -c for create profile, -i for instructions (this runs by default the first time)
-int main() {
-    bool display_intro_msg = false;
+int main()
+{ //number of args, array of null terminated pointers for those args
+
     string check_os_result = check_os();
     cout << check_os_result << endl;
-    
-    if (check_os_result == "Windows") {
-        std::ifstream config_file("config.txt"); 
-        if (config_file.is_open()) {
-            string display_intro_msg_line; 
-            while(getline(config_file, display_intro_msg_line)) { // Go until it's found the line that holds this setting
-                size_t found_intro_true = display_intro_msg_line.find("display_intro = true"); 
-                size_t found_intro_false = display_intro_msg_line.find("display_intro = false");
 
-                if (found_intro_true != string::npos) {
+    if (check_os_result == "Windows")
+    {
+        std::ifstream config_file("config.txt"); //read settings file here 
+        bool display_intro_msg = false; // Initialize to false
+        
+        if (config_file.is_open()) 
+        {
+            string display_intro_msg_line; // The line the setting is on 
+
+            while(getline(config_file, display_intro_msg_line)) 
+            { // Go until it's found the line that holds this setting
+                size_t found = display_intro_msg_line.find("display_intro = true"); 
+                if (read_config(found) == true) 
+                {
                     display_intro_msg = true; 
-                    break; 
-                } else if (found_intro_false != string::npos) {
+                    break;
+                } else 
+                {
                     display_intro_msg = false; 
                     break; 
                 }
             }
             config_file.close();
         }
-
-        if (display_intro_msg == true) {
-            cout << "This should be set to true: " << display_intro_msg << endl; 
-        } else {
-            cout << "This should be set to false: " << display_intro_msg << endl; 
+        if (display_intro_msg == true)
+        {
+            cout << "this should be set to true: " << display_intro_msg <<endl; 
+        } else 
+        {
+            cout << "this should be false" <<endl; 
         }
     }
-
+    //char* death_row[] = {"temp"};
+    //kill_programs(check_os_result, death_row);
     return 0;
 }
+
+bool read_config(size_t found)
+{
+    if (found != string::npos) {
+        return true; 
+    } else {
+        return false; 
+    }
+
+}
+
 //check os, bc the way to kill programs varies 
-string check_os() {
+string check_os() 
+{
     string current_os; //keeping as string in case more os's are added
     #if _WIN32 || _WIN64
         current_os = "Windows";
@@ -55,7 +76,8 @@ string check_os() {
     return current_os;
 };
 
-void kill_programs(string check_os_result, char* death_row[]){
+void kill_programs(string check_os_result, char* death_row[])
+{
         //system("taskkill /F /IM discord.exe >nul 2>&1"); windows, should change this
 };
 
