54f1003109ca1436797af9be3f7f7fde7ef2d522
OblivionNoirV2 <roslyndo79@gmail.com>
2024-03-08 12:45:33 -0600

    finish cmake setup

---
+++ out/build/CMakeFiles/Terminator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Terminator.dir/terminator.cpp.o"
+  "CMakeFiles/Terminator.dir/terminator.cpp.o.d"
+  "Terminator"
+  "Terminator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Terminator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
